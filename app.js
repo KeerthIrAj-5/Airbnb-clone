27455448f7962e167db3b7bb7f306ac21e5d8d84
KeerthIrAj-5 <5503keerthi@gmail.com>
2024-08-09 16:52:24 +0530

    Navbar added

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const mongoose=require("mongoose");
 const Listing=require("./models/listing.js");
 const path=require("path");
 const methodOverride = require('method-override')
- 
+const ejsmate=require("ejs-mate");
+
 // override with POST having ?_method=DELETE
 app.use(methodOverride('_method'))
 
@@ -12,6 +13,8 @@ app.use(methodOverride('_method'))
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.engine("ejs",ejsmate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.listen(3000,()=>{
     console.log("Server is litening to port 3000");

0b792c0a518ad1f254b872281527dd2972860991
KeerthIrAj-5 <5503keerthi@gmail.com>
2024-08-13 02:31:06 +0530

    Error handling

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Listing=require("./models/listing.js");
 const path=require("path");
 const methodOverride = require('method-override')
 const ejsmate=require("ejs-mate");
+const wrapAsync=require("./utils/wrapAsync.js")
+const ExpressError=require("./utils/ExpressError.js");
+const {listingSchema}=require("./schema.js");
 
 // override with POST having ?_method=DELETE
 app.use(methodOverride('_method'))
@@ -16,9 +19,6 @@ app.use(express.urlencoded({extended:true}));
 app.engine("ejs",ejsmate);
 app.use(express.static(path.join(__dirname,"/public")));
 
-app.listen(3000,()=>{
-    console.log("Server is litening to port 3000");
-});
 
 const MONGO_URL="mongodb://127.0.0.1:27017/airbnb";
 main().then(()=>{
@@ -35,6 +35,18 @@ app.get(("/"),(req,res)=>{
     res.send("Hi i am root")
 });
 
+const validateListing=(req,res,next)=>{
+    let {error}=listingSchema.validate(req.body);
+    
+    if(error){
+        let errmsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errmsg);
+    }
+    else{
+        next();
+    }
+}
+
 // app.get(("/test"), async (req,res)=>{
 //     let sl=new Listing({
 //         title:"MyVilla",
@@ -48,48 +60,83 @@ app.get(("/"),(req,res)=>{
 //     res.send("Successful testing");
 // });
 
-app.get(("/listings"),async (req,res)=>{
+app.get(("/listings"),wrapAsync(async (req,res)=>{
     let result=await Listing.find({});
     // .then((res)=>{
     //     console.log(res);
     // })
     res.render("listings/index.ejs",{result});
-});
+}));
 app.get(("/listings/new"),(req,res)=>{
     res.render("listings/new.ejs");
 });
 
-app.post(("/listings"),async (req,res)=>{
+app.post(("/listings"),validateListing,wrapAsync(async (req,res,next)=>{
     //let {title,description,image,price,loaction,country}=req.body;
-    //let listing=req.body;
+    //let listing=req.body)
+    // if(!req.body.listings){
+    //     throw new ExpressError(400,"Send valid data for listing");
+    // }
+    //console.log(req.body);
+    
     const nl=new Listing(req.body.listings);
+    // if(!n1.title){
+    //     throw new ExpressError(400,"Title is missing");
+    // }
+    // if(!n1.description){
+    //     throw new ExpressError(400,"Description is missing");
+    // }
+    // if(!n1.price){
+    //     throw new ExpressError(400,"Price is missing");
+    // }
+    // if(!n1.loaction){
+    //     throw new ExpressError(400,"Location is missing");
+    // }
+    // if(!n1.country){
+    //     throw new ExpressError(400,"Country is missing");
+    // }
     await nl.save();
     res.redirect("/listings");
+}));
 
-});
-
-app.get(("/listings/:id/edit"),async (req,res)=>{
+app.get(("/listings/:id/edit"),wrapAsync(async (req,res)=>{
     let {id}=req.params;
     const list=await Listing.findById(id);
     res.render("listings/edit.ejs",{list});
-});
+}));
 
-app.put(("/listings/:id"),async (req,res)=>{
+app.put(("/listings/:id"),validateListing,wrapAsync(async (req,res)=>{
+    // if(!req.body.listings){
+    //     throw new ExpressError(400,"Send valid data for listing");
+    // }
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,req.body.listings);
     res.redirect(`/listings/${id}`);
-});
+}));
 
-app.delete(("/listings/:id"),async (req,res)=>{
+app.delete(("/listings/:id"),wrapAsync(async (req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect(`/listings`); 
-});
-app.get(("/listings/:id"),async (req,res)=>{
+}));
+app.get(("/listings/:id"),wrapAsync(async (req,res)=>{
     let {id}=req.params;
     console.log(id);
     const list=await Listing.findById(id);
     console.log(list);
     res.render("listings/show.ejs",{list});
+}));
+
+app.all("*",(req,res,next)=>{
+    next(new ExpressError(404,"Page not found"));
 });
 
+app.use((err,req,res,next)=>{
+    let {status=500,message="something went wrong"}=err;
+    //res.status(status).send(message);
+    res.status(status).render("error.ejs",{err});
+});
+
+app.listen(3000,()=>{
+    console.log("Server is litening to port 3000");
+});
